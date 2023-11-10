9a3d7905e2fd95d9074ddf92afe203aaf2b1a3ca
Timothy Coleman <Timbo97@yahoo.com>
2023-11-10 11:50:53 -0500

    completion of excercise

--- app.js
+++ app.js
@@ -6,14 +6,57 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
+var a= 20;
+var b= 4;
+var add = a + b;
+var minus = a - b; 
+var multiply = a * b;
+var dividing = a / b;
+
+/*ANSWERS*/
+
+console.log("Addition:", add);
+console.log("Subtraction:", minus);
+console.log("Multiplication:", multiply);
+console.log("Division:", dividing);
+
 // YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+ 
+/*ANSWERS*/
 
-// YOUR CODE HERE
+console.log("num + str:", 11 + 11 ); // Output: 1111
+console.log("num + str2:", 11 + eleven ); // Output: 11eleven
+console.log("num + isPresent:", 11 + true );//  Output: 12
+console.log("firstName + num:", Frodo + 11); // Output: Frodo11
+console.log("isPresent + str:", true + 11); // Output: true11
+console.log("firstName + lastName:", Frodo + Baggins);//  Output: FrodoBaggins
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
 
-// YOUR CODE HERE
+/* ANSWERS */
+
+console.log("val == str3:", val == str3);//  Output: true
+console.log("val === str3:", val === str3);//  Output: false
+console.log("!isPresent2:", !isPresent2); // Output: true
+console.log('("eleven" == str4 && val >= str3):', ("eleven" == str4 && val >= str3));//  Output: false
+console.log("!isPresent2 || isPresent2:", !isPresent2 || isPresent2);// Output: true
+console.log("0 == false:", 0 == false);//  Output: true
+console.log("0 === false:", 0 === false);//  Output: false
+console.log("0 != false:", 0 != false); // Output: false
+console.log("0 !== false:", 0 !== false); // Output: true
+
+
